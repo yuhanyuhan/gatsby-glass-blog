c79bfbd3e8f42e0211aa6bf428d4c2dd4b524950
yuhanyuhan <yuhanlian@gmail.com>
2022-10-08 17:24:49 +0800

    included: Typescript, Prettier, ESLint

---
+++ eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 2020
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': 2, // This will cause Prettier problems to error
+  },
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,7 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-typescript`, 
     {
       resolve: 'gatsby-source-filesystem',
       options: {
